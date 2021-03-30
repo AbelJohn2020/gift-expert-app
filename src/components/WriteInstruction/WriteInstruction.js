aa580c80ecc34bf2a226b60e6347f610d71335b8
Abel AR <abel_john_14@hotmail.com>
2021-03-29 19:50:35 -0500

    Add encodeUri to the base url

--- src/components/WriteInstruction/WriteInstruction.js
+++ src/components/WriteInstruction/WriteInstruction.js
@@ -5,7 +5,7 @@ const WriteInstruction = ({input}) => {
     const [giftImage, setGiftImage] = useState([]);
 
     const giftImages = async () => {
-        const baseUri = await fetch(`https://api.giphy.com/v1/gifs/search?q=${input}&limit=34&api_key=MUsLPLnbjtFosa0X15JsZEZ3TOahbuOQ`);
+        const baseUri = await fetch(`https://api.giphy.com/v1/gifs/search?q=${encodeURI(input)}&limit=34&api_key=MUsLPLnbjtFosa0X15JsZEZ3TOahbuOQ`);
         const { data } = await baseUri.json();
 
         const getImage = data.map((gift) => {
