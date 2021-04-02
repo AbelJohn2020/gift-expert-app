90b08a4639633e7e9f18f0e91d1eccc9316c23e1
Abel AR <abel_john_14@hotmail.com>
2021-04-01 20:14:35 -0500

    Work with custom hook and set time out

--- src/components/WriteInstruction/WriteInstruction.js
+++ src/components/WriteInstruction/WriteInstruction.js
@@ -1,29 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import getGifts from '../../helpers/getGifts/getGifts';
 import { useFetchGifts } from '../../hooks/useFetchGifts';
 import WriteInstructionStyles from './WriteInstructionStyles';
 
 const WriteInstruction = ({input}) => {
-    const [giftImage, setGiftImage] = useState([]);
-
-    const {loading} = useFetchGifts();
-
-    useEffect(() => {
-        getGifts(input)
-            .then((images) => setGiftImage(images));
-    }, [input]) // si la categoria cambia volvera a ejecutar el useEffect
+    const {data: images, loading} = useFetchGifts({ input });
     
     return (
         <>
             <WriteInstructionStyles>
                 <p className="inputValue">{input}</p>
-                { loading
-                    ? <p>loading...</p>
-                    : <p>Complete</p>
-                }
+                { loading && <p>...loading</p>}
                 <div className="container">
                     {
-                        giftImage.map((img) => (
+                        images.map((img) => (
                             <div key={img.id} className="giftImage">
                                 <img src={img.image} alt={img.title}/>
                                 <p>{img.title}</p>
--- src/hooks/useFetchGifts.js
+++ src/hooks/useFetchGifts.js
@@ -1,17 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import getGifts from '../helpers/getGifts/getGifts';
 
-export const useFetchGifts = () => {
+export const useFetchGifts = ({ input }) => {
   const [state, setstate] = useState({
       data: [],
       loading: true,
   });
 
-  setTimeout(() => {
-    setstate({
-        data: [],
-        loading: false,
-    })
-  }, 3000)
+  useEffect(() => {
+    getGifts(input)
+      .then((images) => {
+        setTimeout(() => {
+          console.log(images);
+          setstate({
+            data: images,
+            loading: false,
+          })
+        }, 3000);
+      })
+  }, [input]) // si la categoria cambia volvera a ejecutar el useEffect
 
   return state;
 };
